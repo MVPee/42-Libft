643e25ae0ec87bfdc626d897fc98d4ce643a727e
MVPee <mariusvanpee@hotmail.be>
2023-08-27 11:53:53 +0200

    Norm

--- libft.h
+++ libft.h
@@ -3,18 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mvan-pee <mvan-pee@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mvpee <mvpee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/20 12:06:46 by mvan-pee          #+#    #+#             */
-/*   Updated: 2023/08/20 13:08:37 by mvan-pee         ###   ########.fr       */
+/*   Updated: 2023/08/27 11:52:36 by mvpee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
 
 void	*ft_memset(void *ptr, int value, size_t count);
 void	*ft_bzero(void *ptr, size_t count);
