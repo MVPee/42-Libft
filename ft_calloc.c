e64c4ab5a94df2ae73037516d25511109829d23c
MVP <you@example.com>
2023-11-01 08:39:50 +0100

    Norm calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mvpee <mvpee@19.be>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 09:41:49 by mvpee             #+#    #+#             */
-/*   Updated: 2023/11/01 08:33:58 by mvpee            ###   ########.fr       */
+/*   Updated: 2023/11/01 08:38:17 by mvpee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*mem;
 
 	if (nmemb && SIZE_MAX / nmemb < size)
-    	return NULL;
+		return NULL;
 	mem = (void *)malloc(nmemb * size);
 	if (!mem)
 		return (NULL);
